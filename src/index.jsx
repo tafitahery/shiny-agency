41e06466a618790f3e309d3bf9fc74138c06a5db
Tafita Hery <tafitahery@gmail.com>
2022-07-03 13:00:29 +0300

    Scoper votre css avec styled components

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { createGlobalStyle } from 'styled-components'
 import Error from './components/Error'
 import Header from './components/Header'
 import Freelances from './pages/Freelances'
@@ -8,10 +9,17 @@ import Home from './pages/Home'
 import Result from './pages/Results'
 import Survey from './pages/Survey'
 
+const GlobalStyle = createGlobalStyle`
+  div {
+    font-family: 'Trebuchet MS', Helvetica, sans-serif;
+  }
+`
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <GlobalStyle />
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
